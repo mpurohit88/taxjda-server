26e9350326c6cfdafa4be6fc01cc0b51ea1fd8c0
mukesh purohit <mpurohit88@gmail.com>
2019-12-14 23:12:46 +0530

    url changed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: { fileSize: 100000000 } });
 
 app.use('/api/upload', upload.any(), function (req, res, nex) {
   try {
