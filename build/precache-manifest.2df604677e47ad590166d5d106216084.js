41909262cb08c1a5c6c2365e2b5ad40f2df2ddb4
mukesh purohit <mpurohit88@gmail.com>
2019-12-14 22:18:33 +0530

    first commit

---
+++ build/precache-manifest.2df604677e47ad590166d5d106216084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5597d3c4c81bdf5c2aeef6d4909b22f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d000cb011457729a3be",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "30b5f601a14a9c3f22e6",
+    "url": "/static/js/2.03e44fdc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.03e44fdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d000cb011457729a3be",
+    "url": "/static/js/main.6de8d604.chunk.js"
+  },
+  {
+    "revision": "3e2764314f5ba17c5a67",
+    "url": "/static/js/runtime-main.93c1fce8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,48 @@
+
+/* eslint-disable no-console */
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+app.use('/build', express.static(path.join(__dirname, 'build')));
+
+app.use('/api/upload', function (req, res, nex) {
+  try {
+    console.log("uploaded....");
+    res.send("uploaded"); // Set disposition and send it.
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(function (error, req, res, next) {
+  // Any request to this server will get here, and will send an HTTP
+  // response with the error message 'woops'
+  console.log("Server Error....", error);
+
+  res.status(error.statusCode).send("error");
+});
+
+let port = 5000;
+
+app.set('port', port);
+
+/** Create HTTP server. */
+const server = http.createServer(app);
+/** Listen on provided port, on all network interfaces. */
+server.listen(port, () => console.log(`Server Running on port ${port}`));
