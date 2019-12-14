63d0e44382197f5e9ac4d40ae1f0f2917941c857
mukesh purohit <mpurohit88@gmail.com>
2019-12-14 23:14:08 +0530

    url changed

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '15360mb', type: 'application/json' }))
+app.use(bodyParser.urlencoded({ limit: '15360mb', extended: true, type: 'application/json', parameterLimit: 5000000 }))
 
 app.use(cors());
 
