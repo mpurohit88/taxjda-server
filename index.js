a8a44c4145e19d4f879aa543eac2e976e052c9c7
mukesh purohit <mpurohit88@gmail.com>
2020-02-19 18:28:46 +1345

    email changes

--- index.js
+++ index.js
@@ -51,6 +51,13 @@ app.use('/api/upload', function (req, res, next) {
   }
 });
 
+const mailer = require('./mailer');
+
+app.post('/api/sendEmail', function (req, res, next) {
+  mailer.contactUsEmail(req.body);
+  res.send("email send");
+});
+
 app.use(function (error, req, res, next) {
   // Any request to this server will get here, and will send an HTTP
   // response with the error message 'woops'
---
+++ mailer.js
@@ -0,0 +1,23 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('');
+
+let contactUsEmail = (body) => {
+    const msg = {
+        to: 'mpurohit88@gmail.com', //'info@cajaindhingra.com',
+        from: {
+            email: "info@cajaindhingra.com",
+            name: "cajaindhingra"
+        },
+        subject: body.subject,
+        text: body.message,
+        html: 'Subject: ' + body.subject + ' Message: ' + body.message + ' mobileNo: ' + body.phone + ' email address: ' + body.email + ' name: ' + body.name,
+    };
+    try {
+        sgMail.send(msg);
+    } catch (e) {
+        console.log('error...', e);
+    }
+}
+module.exports = {
+    contactUsEmail
+}
