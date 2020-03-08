cef72094e958ca94ed0a8a6554c4779e5dffc093
mukesh purohit <mpurohit88@gmail.com>
2020-03-08 19:53:19 +1345

    Static file changes

---
+++ build/precache-manifest.c7f3dd1e729888533514347714e5717b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5a1ea7d33ba65bd928b9c578328518a",
+    "url": "/static/media/logo2.c5a1ea7d.png"
+  },
+  {
+    "revision": "72ec95a74868db74dc14",
+    "url": "/static/css/main.1b0e070c.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "99fd5c49f66aefe02908fffb0bc9d0e9",
+    "url": "/static/media/digi.99fd5c49.jpg"
+  },
+  {
+    "revision": "e3125adfb9b03b6d611e",
+    "url": "/static/js/2.25fe3926.chunk.js"
+  },
+  {
+    "revision": "f74005b6e0bd52003f6197e08726ea1e",
+    "url": "/static/media/CALogo.f74005b6.png"
+  },
+  {
+    "revision": "ba3d8c2a29499e3690b16b1e31c7c8bd",
+    "url": "/static/media/cta-bg-img.ba3d8c2a.jpg"
+  },
+  {
+    "revision": "02674537535001827ed67d83cf420aee",
+    "url": "/static/media/banner-bg.02674537.jpg"
+  },
+  {
+    "revision": "8f4583673b91e580b0e4958b5d7a0196",
+    "url": "/static/media/slide-bg-2.8f458367.jpg"
+  },
+  {
+    "revision": "956c4b1d7b2ee350663fcf01aaf4fefa",
+    "url": "/static/media/slide-bg-3.956c4b1d.jpg"
+  },
+  {
+    "revision": "fa7d963f2f34aeb40dc09d208f00a8e8",
+    "url": "/static/media/slide-bg-1.fa7d963f.jpg"
+  },
+  {
+    "revision": "5e21238dc532def29a390680c75636c0",
+    "url": "/static/media/video.5e21238d.jpg"
+  },
+  {
+    "revision": "0e00b0022cd432171f67614319459460",
+    "url": "/static/media/logo.0e00b002.png"
+  },
+  {
+    "revision": "72ec95a74868db74dc14",
+    "url": "/static/js/main.9412d032.chunk.js"
+  },
+  {
+    "revision": "499bdb391a8e1f54dc0bc218b23a334f",
+    "url": "/static/media/rakeshGupta.499bdb39.jpg"
+  },
+  {
+    "revision": "fd924c2c57d4b45fd5604d0dae9679e6",
+    "url": "/static/media/sekharnigam.fd924c2c.jpg"
+  },
+  {
+    "revision": "26ade050dfb9966633e3888e241e9662",
+    "url": "/static/media/ajaygupta.26ade050.jpg"
+  },
+  {
+    "revision": "2aaaa3643bea91fc4437a1e0a6405ed9",
+    "url": "/static/media/PankajKumpawat.2aaaa364.jpg"
+  },
+  {
+    "revision": "8a047627d1df3f480f2b9812ab6817a6",
+    "url": "/static/media/kcagarwal.8a047627.png"
+  },
+  {
+    "revision": "cc774255254ecea28bf470a871f48259",
+    "url": "/static/media/founder11.cc774255.png"
+  },
+  {
+    "revision": "9d27f8849ec800c3a0e2c726106aa1f1",
+    "url": "/static/media/founder22.9d27f884.png"
+  },
+  {
+    "revision": "0fd6505516102cc12101d5d0d571fcee",
+    "url": "/static/media/efforts.0fd65055.jpg"
+  },
+  {
+    "revision": "a4dc1cb4f4b25253461b138dacbea51a",
+    "url": "/static/media/enterpreniourship.a4dc1cb4.jpg"
+  },
+  {
+    "revision": "015f1e990102d4e6235e350f6d775a9f",
+    "url": "/static/media/successful.015f1e99.jpg"
+  },
+  {
+    "revision": "e3125adfb9b03b6d611e",
+    "url": "/static/css/2.3ad3650c.chunk.css"
+  },
+  {
+    "revision": "d95c61ffe8910f466fea4b3af2cfb4ec",
+    "url": "/index.html"
+  }
+];
--- mailer.js
+++ mailer.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('');
+sgMail.setApiKey('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');
 
 let contactUsEmail = (body) => {
     const msg = {
